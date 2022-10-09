6bd6be1336d5be1dcd908e3ac1c21f7a525e9dee
Kevin <47770053+kevinwchen@users.noreply.github.com>
2022-10-09 17:32:38 +1100

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,7 +1,17 @@
 const removeFromArray = function() {
-  let inputArray = arguments[0];
-  console.log(arguments[1]);
+  const args = Array.from(arguments); // convert to array
+  const inputArray = args[0]; // first arg is the array to remove from
+  const toRemove = args.slice(1); // remaining args are elements be removed
+
+  // Loop for each element to be removed
+  for (let i = 0; i < toRemove.length; i++) {
+    // Loop while the element to be removed exists in the array
+    while (inputArray.indexOf(toRemove[i]) != -1) {
+      inputArray.splice(inputArray.indexOf(toRemove[i]),1);
+      console.log(inputArray);
+    }
+  }
+  return inputArray;
 };
-removeFromArray([1,2,3,4])
 // Do not edit below this line
 module.exports = removeFromArray;
