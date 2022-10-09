a946eac01b986ea86c58e7530e19628534a29435
Kevin <47770053+kevinwchen@users.noreply.github.com>
2022-10-09 12:25:58 +1100

    Enable all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,7 @@
 const removeFromArray = function() {
-
+  let inputArray = arguments[0];
+  console.log(arguments[1]);
 };
-
+removeFromArray([1,2,3,4])
 // Do not edit below this line
 module.exports = removeFromArray;
