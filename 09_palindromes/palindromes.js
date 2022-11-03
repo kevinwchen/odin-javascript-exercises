a46d704baf4c12b2fcc50caed2698f9ca824787d
Kevin Chen <47770053+kevinwchen@users.noreply.github.com>
2022-11-04 10:53:24 +1100

    Complete palindrom exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,8 +1,15 @@
 const palindromes = function (inputString) {
-// Remove all punctuation and spaces from string
-// Convert to lowercase
-// Generated reversed string and compare to ordered string
-};
+  // Remove all punctuation and spaces from string
+  // Convert to lowercase
+  const cleanString = inputString.replace(/[^\w]|_/g, "").toLowerCase();
 
+  // Compare reverse string to ordered string
+  for (i = 0; i < cleanString.length; i++) {
+    if (cleanString[i] !== cleanString[cleanString.length-1-i]) {
+      return false;
+    }
+  }
+  return true;
+};
 // Do not edit below this line
 module.exports = palindromes;
