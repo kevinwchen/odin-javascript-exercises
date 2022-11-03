327515289a8af2ec98f5b1d929b34d4adf5202f8
Kevin Chen <47770053+kevinwchen@users.noreply.github.com>
2022-11-03 23:04:28 +1100

    Comment solution to palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (inputString) {
+// Remove all punctuation and spaces from string
+// Convert to lowercase
+// Generated reversed string and compare to ordered string
 };
 
 // Do not edit below this line
