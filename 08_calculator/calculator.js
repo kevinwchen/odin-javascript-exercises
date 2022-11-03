da62914a08cfb778dde45a459d4f2f3b5a20481e
Kevin Chen <47770053+kevinwchen@users.noreply.github.com>
2022-11-03 22:57:27 +1100

    Complete calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numArray) {
+	let sum = 0;
+  for (const num of numArray) {
+    sum += num;
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(numArray) {
+	let product = numArray[0];
+  for (let i = 1; i < numArray.length; i++) {
+    product *= numArray[i];
+  }
+  return product;
 };
 
-const power = function() {
-	
+multiply([1,2,3])
+
+const power = function(a, b) {
+  let power = a;
+  if (b === 0) {return 1};
+
+	for (let i = 1; i < b; i++) {
+    power *= a;
+  }
+  return power;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let fact = 1;
+
+  for (i = 2; i <= num; i++) {
+    fact *= i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
