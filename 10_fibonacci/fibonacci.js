fd97186ce2316b4146253f391f40ef384fcae37b
Kevin Chen <47770053+kevinwchen@users.noreply.github.com>
2022-11-04 11:25:25 +1100

    Complete fibonacci sequence exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+  if (n < 0) {return "OOPS"};
 
+  if (n == 0) {return 0};
+  if (n == 1) {return 1};
+
+  let fib = [0, 1];
+  
+  for (let i = 2; i <= n; i++) {
+    fib.push(fib[fib.length-1] + fib[fib.length-2]);
+  }
+  
+  return fib[fib.length-1];
 };
 
 // Do not edit below this line
