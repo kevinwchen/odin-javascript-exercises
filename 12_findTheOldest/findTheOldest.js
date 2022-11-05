766de436f32f739d2a074eb3808e12e2292b8fae
Kevin Chen <47770053+kevinwchen@users.noreply.github.com>
2022-11-05 13:08:15 +1100

    Complete findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findAge = function(person) {
+  if (person.yearOfDeath == undefined) {
+    const thisYear = new Date().getFullYear();
+    return thisYear - person.yearOfBirth; // Return age if alive
+  } else {
+    return person.yearOfDeath - person.yearOfBirth; // Return age if dead
+  }
+}
 
+const findTheOldest = function(people) {
+  const oldest = people.reduce((lastPerson, nextPerson) => {
+    // Compare age of last person and next person
+    // Return older person and repeat for next object
+    if (findAge(lastPerson) < findAge(nextPerson)) {
+      return nextPerson;
+    } else {
+      return lastPerson;
+    }
+  }, people[0]);
+  return oldest;
 };
 
 // Do not edit below this line
